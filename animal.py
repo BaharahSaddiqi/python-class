4db3fa6084e1bf0482522d17bcab5fd476243af5
baharah <baharahsaddiqi@gmail.com>
2025-07-02 23:38:03 +0200

    " class Animal "

---
+++ animal.py
@@ -0,0 +1,23 @@
+class Animal:
+     def speak(self):
+         pass
+class Dog(Animal):
+     def speak(self):
+        print("Woffff")
+class Cat(Animal):
+  def speak(self):
+        print("MIAUUUU")
+
+    
+class Cow(Animal):  
+   def speak(selF):
+        
+        print("Mahhhhhh")
+
+animals=[Dog(), Cat(),Cow()]
+for i in animals:
+    i.speak()
+
+
+
+
