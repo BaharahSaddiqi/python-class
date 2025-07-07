6b8599b8f65ac8d5a86bc35dccc408b9a36cbcb4
baharah <baharahsaddiqi@gmail.com>
2025-07-08 00:11:11 +0200

    "update with comments"

--- project/base.py
+++ project/base.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
+
+#base class
 class PaymentMethod(ABC):
     def __init__(self,accunt_holder,currency):
      self.accunt_holder= accunt_holder
      self.currency = currency
-    
+    #the two basic methods
     def authorize_payment(self):
       pass
 
@@ -13,15 +15,16 @@ class PaymentMethod(ABC):
 
 
 
-
+#first child class
 
 class CreditCardpayment(PaymentMethod):
     def __init__(self, accunt_holder, currency,card_number):
+       #parent meethod
        super().__init__(accunt_holder, currency)
        self.card_number=card_number
    
     
-
+#  two common methods 
     def authorize_payment(self):
      print(f"[CreditCardpayment] CreditCard is belong to{self.accunt_holder} and your card number is{self.card_number}")
 
@@ -33,7 +36,7 @@ class CreditCardpayment(PaymentMethod):
 
 
 
-
+#second child class
 class Paypal(PaymentMethod):
     def __init__(self, accunt_holder, currency,email):
         super().__init__(accunt_holder, currency)
@@ -52,8 +55,7 @@ class Paypal(PaymentMethod):
 
 
 
-
-
+#third child class
 class Crypto(PaymentMethod):
      def __init__(self, accunt_holder, currency,wallet_adress):
           super().__init__(accunt_holder, currency)
@@ -80,7 +82,7 @@ if __name__=="__main__":
       payments=[CreditCardpayment("Bahar" ,"USD","345_6755_654"),
                Paypal("Fardin","EUR","fardin23@gmail.com"),
                Crypto("Umair","BT","3e5rt6tz")]
-      
+      # call the methods 
       for payment in payments:
            payment.authorize_payment()
            payment.process_payment()
